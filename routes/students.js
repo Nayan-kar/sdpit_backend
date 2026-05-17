ef92f8fdfba65af17cc3f34469bce40fb575bcd9
Nayan-Kar <nayankumarkar2004@gmail.com>
2026-05-17 08:59:01 +0530

    Added missing video and student routes

--- routes/students.js
+++ routes/students.js
@@ -1,9 +1,20 @@
 const express = require('express');
-const router = express.Router();
-const { getStudents, updateStudentStatus } = require('../controllers/studentController');
-const { verifyToken, verifyAdmin } = require('../middleware/auth');
 
-router.get('/', verifyAdmin, getStudents);
-router.put('/:id/status', verifyAdmin, updateStudentStatus);
+const router = express.Router();
+
+const {
+
+  getStudents,
+  updateStudentStatus
+
+} = require('../controllers/studentController');
+
+const auth = require('../middleware/auth');
+
+// GET ALL STUDENTS
+router.get('/', auth, getStudents);
+
+// BLOCK / UNBLOCK STUDENT
+router.put('/:id', auth, updateStudentStatus);
 
 module.exports = router;
--- routes/videos.js
+++ routes/videos.js
@@ -1,11 +1,28 @@
 const express = require('express');
-const router = express.Router();
-const { getVideosByCourseId, createVideo, updateVideo, deleteVideo } = require('../controllers/videoController');
-const { verifyToken, verifyAdmin } = require('../middleware/auth');
 
-router.get('/course/:courseId', verifyToken, getVideosByCourseId);
-router.post('/', verifyAdmin, createVideo);
-router.put('/:id', verifyAdmin, updateVideo);
-router.delete('/:id', verifyAdmin, deleteVideo);
+const router = express.Router();
+
+const {
+
+  getVideosByCourseId,
+  createVideo,
+  updateVideo,
+  deleteVideo
+
+} = require('../controllers/videoController');
+
+const auth = require('../middleware/auth');
+
+// GET VIDEOS BY COURSE
+router.get('/:courseId', auth, getVideosByCourseId);
+
+// CREATE VIDEO
+router.post('/', auth, createVideo);
+
+// UPDATE VIDEO
+router.put('/:id', auth, updateVideo);
+
+// DELETE VIDEO
+router.delete('/:id', auth, deleteVideo);
 
 module.exports = router;
