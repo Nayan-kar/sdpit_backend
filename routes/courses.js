a3ada4f619d12d5ce36edcc31d951069ecbd6441
Nayan-Kar <nayankumarkar2004@gmail.com>
2026-05-17 10:16:00 +0530

    Fixed courses auth middleware

--- routes/courses.js
+++ routes/courses.js
@@ -1,12 +1,32 @@
 const express = require('express');
-const router = express.Router();
-const { getCourses, getCourseById, createCourse, updateCourse, deleteCourse } = require('../controllers/courseController');
-const { verifyToken, verifyAdmin } = require('../middleware/auth');
 
-router.get('/', verifyToken, getCourses);
-router.get('/:id', verifyToken, getCourseById);
-router.post('/', verifyAdmin, createCourse);
-router.put('/:id', verifyAdmin, updateCourse);
-router.delete('/:id', verifyAdmin, deleteCourse);
+const router = express.Router();
+
+const {
+
+  getCourses,
+  getCourseById,
+  createCourse,
+  updateCourse,
+  deleteCourse
+
+} = require('../controllers/courseController');
+
+const auth = require('../middleware/auth');
+
+// GET ALL COURSES
+router.get('/', auth, getCourses);
+
+// GET SINGLE COURSE
+router.get('/:id', auth, getCourseById);
+
+// CREATE COURSE
+router.post('/', auth, createCourse);
+
+// UPDATE COURSE
+router.put('/:id', auth, updateCourse);
+
+// DELETE COURSE
+router.delete('/:id', auth, deleteCourse);
 
 module.exports = router;
