a11199a93206039d4f61ac0607fe6c9c6215781c
Nayan-Kar <nayankumarkar2004@gmail.com>
2026-05-17 09:37:03 +0530

    Fixed video route callback issue

--- routes/videos.js
+++ routes/videos.js
@@ -2,27 +2,20 @@ const express = require('express');
 
 const router = express.Router();
 
-const {
-
-  getVideosByCourseId,
-  createVideo,
-  updateVideo,
-  deleteVideo
-
-} = require('../controllers/videoController');
+const videoController = require('../controllers/videoController');
 
 const auth = require('../middleware/auth');
 
 // GET VIDEOS
-router.get('/:courseId', auth, getVideosByCourseId);
+router.get('/:courseId', auth, videoController.getVideosByCourseId);
 
 // CREATE VIDEO
-router.post('/', auth, createVideo);
+router.post('/', auth, videoController.createVideo);
 
 // UPDATE VIDEO
-router.put('/:id', auth, updateVideo);
+router.put('/:id', auth, videoController.updateVideo);
 
 // DELETE VIDEO
-router.delete('/:id', auth, deleteVideo);
+router.delete('/:id', auth, videoController.deleteVideo);
 
 module.exports = router;
